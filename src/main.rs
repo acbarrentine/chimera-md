76b678a10d4922c38a67f0d8c91bf85952e60bb0
acbarrentine <acbarrentine@gmail.com>
2024-05-26 10:53:37 -0500

    Added tokio. Can read and convert a single markdown file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
-//use markdown::to_html;
+use tokio::{fs, io::{stdout, AsyncWriteExt}};
 
-fn main() {
-    println!("{}", markdown::to_html("## Hello, *world*!"));
+#[tokio::main]
+async fn main() {
+    match fs::read_to_string("/Users/acbarrentine/Documents/MKScript/MKScript.md").await {
+        Ok(md_content) => {
+            let md = markdown::to_html(md_content.as_str());
+            stdout().write_all(md.as_bytes()).await.unwrap();
+        },
+        Err(msg) => {
+            eprintln!("Could not read source: {}", msg);
+        }
+    }
 }
