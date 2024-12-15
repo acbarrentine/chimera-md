3068d537f158e4e0239df2f609559f4f0b8293da
acbarrentine <acbarrentine@gmail.com>
2024-12-15 13:07:51 -0600

    Try #3. No improvement

--- docker/mac-build.sh
+++ docker/mac-build.sh
@@ -8,3 +8,5 @@ echo "Building docker image for Mac version: $version";
 cargo build --release --target=aarch64-unknown-linux-gnu
 # docker build -t acbarrentine/chimera-md-mac:$version -f Dockerfile .;
 docker build -t acbarrentine/chimera-md-mac:$version -f native-dockerfile .;
+docker container rm -f chimera-mac
+docker run --hostname=515e12074420 --env=PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin --volume=/Users/acbarrentine/Source/dismal.ink/chimera.toml:/data/chimera.toml --volume=/Users/acbarrentine/Source/dismal.ink/image-sizes.toml:/data/image-sizes.toml --volume=/Users/acbarrentine/Source/dismal.ink/www:/data/www --volume=/Users/acbarrentine/Source/dismal.ink/home:/data/home --volume=/Users/acbarrentine/Source/dismal.ink/log:/data/log --volume=/Users/acbarrentine/Source/dismal.ink/search:/data/search --volume=/Users/acbarrentine/Source/dismal.ink/templates:/data/template --volume=/Users/acbarrentine/Paintings:/data/home/media/Paintings:ro --volume=/Users/acbarrentine/Paintings-archived:/data/home/media/Archive:ro --workdir=/ --restart=no --name chimera-mac --runtime=runc -p 8080:8080 -d acbarrentine/chimera-md-mac:latest
